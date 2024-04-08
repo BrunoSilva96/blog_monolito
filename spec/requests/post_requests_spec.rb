3c16ee608f7835befd8df04e914a65395acd3f24
Bruno Silva <brunosilvabrait@gmail.com>
2024-04-08 18:50:55 -0300

    adding update and delete test

--- spec/requests/post_requests_spec.rb
+++ spec/requests/post_requests_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe '/posts', type: :request do
     end
 
     context 'with valid params' do
-      it 'create a new post' do
+      it 'create a new Post' do
         post_params = FactoryBot.attributes_for(:post)
 
         post posts_path, params: { post: { text: post_params[:text], user_id: user.id } }
@@ -20,7 +20,7 @@ RSpec.describe '/posts', type: :request do
     end
 
     context 'with invalid params' do
-      it 'does not create a new post' do
+      it 'does not create a new Post' do
         post_params = { text: '' }
 
         post posts_path, params: { post: { text: post_params[:text], user_id: user.id } }
@@ -32,19 +32,56 @@ RSpec.describe '/posts', type: :request do
   end
 
   describe 'PUT /posts' do
-    context 'update post' do
-      let!(:user) { create(:user) }
-      let!(:post) { create(:post, user:) }
+    let!(:user) { create(:user) }
+    let!(:post) { create(:post, user:) }
 
+    context 'with valid params' do
       before do
         sign_in user
       end
 
-      it 'updates post with valid params' do
-        put posts_path, params: { post: { text: 'New post text' } }
+      it 'updates Post' do
+        put posts_path, params: { post: { id: post.id, text: 'New post text' } }
 
         expect(response).to redirect_to(posts_path)
         expect(post.reload.text).to eq('New post text')
+      end
+    end
+
+    context 'invalid params' do
+      it 'does not update Post' do
+        put posts_path, params: { post: { id: post.id, text: nil } }
+
+        expect { post.reload }.not_to(change { post.text })
+      end
+    end
+  end
+
+  describe 'DELETE /posts' do
+    let!(:user1) { create(:user) }
+    let!(:post1) { create(:post, user: user1) }
+    let!(:user2) { create(:user) }
+    let!(:post2) { create(:post, user: user2) }
+
+    before do
+      sign_in user1
+    end
+
+    context 'Delete post' do
+      it 'delete Post with success' do
+        delete posts_path, params: { post: { id: post1.id } }
+
+        expect(response).to have_http_status(:no_content)
+        expect(Post.count).to eq(1)
+      end
+    end
+
+    context 'when user tries to delete another users post' do
+      it 'does not delete the post' do
+        delete posts_path, params: { post: { id: post2.id, user: user2.id } }
+
+        expect(response).to have_http_status(401)
+        expect(Post.count).to eq(2)
       end
     end
   end
