0e513a05f916b34010aee31f5c052994cb6e32e9
Bruno Silva <brunosilvabrait@gmail.com>
2024-04-08 18:50:17 -0300

    adjustment in the part of loading post, destroying and updating

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
-  before_action :load_post, only: %i[update]
+  before_action :load_post, only: %i[update destroy]
 
   def create
     @post = Post.new(post_params)
@@ -20,10 +20,18 @@ class PostsController < ApplicationController
     redirect_to posts_path if @post.save!
   end
 
+  def destroy
+    if @post.nil?
+      head :unauthorized
+    else
+      @post.destroy!
+    end
+  end
+
   private
 
   def load_post
-    @post = current_user.posts.find_by_id(id: params[:id])
+    @post = current_user.posts.find_by(id: params[:post][:id])
   end
 
   def post_params
