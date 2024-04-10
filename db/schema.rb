0b8d6341104f1b5945113eb56aa1a4b510333ea9
Bruno Silva <brunosilvabrait@gmail.com>
2024-04-10 20:34:18 -0300

    adding new parameter for the user

--- db/schema.rb
+++ db/schema.rb
@@ -38,6 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_09_163419) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
