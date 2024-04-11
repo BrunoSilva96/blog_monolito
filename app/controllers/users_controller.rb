5639665108f451e7a7968b9fb08e9b371b2fd2e5
Bruno Silva <brunosilvabrait@gmail.com>
2024-04-11 16:27:49 -0300

    temporary solution for changing password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,19 @@ class UsersController < ApplicationController
     @user.save!
   end
 
+  def update_password
+    if !current_password_params[:current_password].empty? && current_user.valid_password?(current_password_params[:current_password])
+      if current_user.update(password_params)
+        bypass_sign_in(current_user)
+        redirect_to password_settings_path, notice: 'Sua senha foi atualizados com sucesso!'
+      else
+        render password_settings_path
+      end
+    else
+      redirect_to password_settings_path, notice: 'Você precisa informar sua senha atual para trocar de senha'
+    end
+  end
+
   private
 
   def load_user
