b44451af8fee47c5945b1605bfbdf5b51d958384
Bruno Silva <brunosilvabrait@gmail.com>
2024-04-11 16:28:23 -0300

    shipping and email configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,6 +19,16 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   user_name: 'a281ed1effc6ec',
+  #   password: '********ae1',
+  #   address: 'sandbox.smtp.mailtrap.io',
+  #   host: 'sandbox.smtp.mailtrap.io',
+  #   port: '2525',
+  #   authentication: :login
+  # }
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp/caching-dev.txt').exist?
@@ -60,6 +70,8 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+
+  config.i18n.default_locale = 'pt-BR'
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
