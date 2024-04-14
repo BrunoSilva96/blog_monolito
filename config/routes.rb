7c87457878b3131f3ec562c6404e1fd9771dfc3a
Bruno Silva <brunosilvabrait@gmail.com>
2024-04-14 19:47:15 -0300

    settings route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  resources :settings, only: [:index] do
+    collection do
+      get :password
+      patch :update_user
+      patch :update_password
+    end
+  end
+
   resource :posts, only: %i[show create update destroy] do
     get :index, on: :collection
   end
