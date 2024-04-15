00536eb587caf83bdfcba4cd9aa010a41a58c365
Bruno Silva <brunosilvabrait@gmail.com>
2024-04-15 20:09:14 -0300

    fix bug post_params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+
     @post.user_id = current_user.id
 
     if @post.save
@@ -22,7 +23,7 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
+    @post.update(post_params_update)
 
     redirect_to posts_path if @post.save!
   end
@@ -39,7 +40,11 @@ class PostsController < ApplicationController
     head :unauthorized if @post.nil?
   end
 
-  def post_params
+  def post_params_update
     params.require(:post).permit(:text)
+  end
+
+  def post_params
+    params.permit(:text, :tag_ids)
   end
 end
