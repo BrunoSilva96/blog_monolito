1d70f99c88dab9bc4fd4d73e1a154d2230ba19e2
Bruno Silva <brunosilvabrait@gmail.com>
2024-05-13 15:48:12 -0300

    authentication for comments

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,8 @@ module PostsHelper
   def user_post_delete?(post, current_user)
     user_signed_in? && current_user == post.user
   end
+
+  def user_comment?(post, comment, current_user)
+    user_signed_in? && current_user == comment.user || current_user == post.user
+  end
 end
